0ef2f5b8a8e27ea4df3b6c96de9375a1c6317fa2
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-06-26 00:21:59 -0300

    fix: removendo entrada.routes

--- backend/api-js/index.ts
+++ backend/api-js/index.ts
@@ -5,6 +5,7 @@ import { UserRepository } from './src/repository/usuario.repository';
 import { PlacaRepository } from './src/repository/placa.repository';
 import { usuarioRouter } from './routes/user.routes';
 import { placaRouter } from './routes/placa.routes';
+import { EntradaRepository } from './src/repository/entrada.repository';
 const app = express();
 app.use(express.json())
 
@@ -22,9 +23,10 @@ connect().then(db => api(db)).catch(err=>console.error("Falha ao conectar no mys
 function api(db:Connection) {
   const userRepository = new UserRepository(db)
   const placaRepository = new PlacaRepository(db)
+  const entradaRepository = new EntradaRepository(db)
   
   app.use("/usuario", usuarioRouter(userRepository))
-  app.use("/placa", placaRouter(placaRepository))
+  app.use("/placa", placaRouter(placaRepository, entradaRepository))
 
   app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
