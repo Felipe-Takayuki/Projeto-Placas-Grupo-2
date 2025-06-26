57807b2ab225e4be2e1c9b5960c7b2256276ce81
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-06-26 00:21:15 -0300

    fix: adicionando entradaRepository

--- backend/api-js/routes/placa.routes.ts
+++ backend/api-js/routes/placa.routes.ts
@@ -2,8 +2,9 @@
 import { Router } from 'express';
 import { PlacaRepository } from '../src/repository/placa.repository';
 import { PlacaController } from '../src/controller/placa.controller';
-export function placaRouter(placaRepository: PlacaRepository) {
-  const placaController = new PlacaController(placaRepository)
+import { EntradaRepository } from '../src/repository/entrada.repository';
+export function placaRouter(placaRepository: PlacaRepository, entradaRepository: EntradaRepository) {
+  const placaController = new PlacaController(placaRepository,entradaRepository)
   const router = Router();
   router.post('/',(req,res) => placaController.create(req, res));
   router.delete('/:id',(req, res)=> placaController.delete(req, res));
